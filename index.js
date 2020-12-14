66d0d6de24b4a70f24e13908f7ec3d7c07c54247
DablioZe <wyskzito@gmail.com>
2020-12-14 04:41:40 -0300

    Add express server

---
+++ index.js
@@ -0,0 +1,12 @@
+/* a basic server with express */
+
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+
+app.use(express.static("./"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+http.listen(3000)
