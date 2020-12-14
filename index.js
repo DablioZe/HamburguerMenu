ecb4f000132d243794eeb24fbf2bb6b5bdd772b4
DablioZe <wyskzito@gmail.com>
2020-12-14 05:03:03 -0300

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const http = require("http").createServer(app);
+const http = require("http").Server(app);
 
 app.use(express.static("./"));
 app.get("/", (req, res) => {
